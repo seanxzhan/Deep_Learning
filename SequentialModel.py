446111bee287c14864734e7de5b6982b7ec196be
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-03 17:24:58 -0400

    Added training data, added training module

--- SequentialModel.py
+++ SequentialModel.py
@@ -1,12 +1,56 @@
+import keras
+from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
 import numpy as np
-from scipy import ndimage
-import matplotlib.pyplot as plt
+from random import randint
+from sklearn.preprocessing import MinMaxScaler
 
+train_labels = []
+train_samples = []
+# generating random data
+for i in range(1000):
+    # young people with no side effect
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(0)
+
+    # old people with side effect
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(1)
+
+for i in range(50):
+    # young people with side effect
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(1)
+
+    # old people without side effect
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(0)
+
+train_samples = np.array(train_samples)
+train_labels = np.array(train_labels)
+
+# to make net easier to learn
+scaler = MinMaxScaler(feature_range=(0, 1))
+# reshaping data into -1, 1
+scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1, 1))
+
+# this model has 2 hidden layers of 16 nodes and 32 nodes
 model = Sequential([
     # dense layers are fully connected layers
-    # 5 nodes, shape of data,  activation function
-    Dense(5, input_shape=(3,), activation="relu"),
+    # 16 nodes, shape of data, name of activation function
+    Dense(16, input_shape=(1,), activation="relu"),
+    Dense(32, activation="relu"),
     Dense(2, activation="softmax")
 ])
+
+# Adam is an optimizer, loss function, what's printed out when we print out our model
+model.compile(Adam(lr=.0001), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+# numpy array that holds data, labels, batch size, epoch, shuffle, how much output when we see
+model.fit(scaled_train_samples, train_labels, batch_size=10, epochs=20, shuffle=True, verbose=2)
