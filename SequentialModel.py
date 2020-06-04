dee8730381d9fb903024d5cd0c768e40da6fb5e9
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-04 16:05:35 -0400

    Added testing data and predictions

--- SequentialModel.py
+++ SequentialModel.py
@@ -40,6 +40,7 @@ train_labels = np.array(train_labels)
 scaler = MinMaxScaler(feature_range=(0, 1))
 # reshaping data into -1, 1
 scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1, 1))
+print(scaled_train_samples)
 
 # this model has 2 hidden layers of 16 nodes and 32 nodes
 model = Sequential([
@@ -52,5 +53,28 @@ model = Sequential([
 
 # Adam is an optimizer, loss function, what's printed out when we print out our model
 model.compile(Adam(lr=.0001), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+# model.optimizer.lr = 0.01     # this is how to update learning rate after model is compiled
 # numpy array that holds data, labels, batch size, epoch, shuffle, how much output when we see
-model.fit(scaled_train_samples, train_labels, batch_size=10, epochs=20, shuffle=True, verbose=2)
+# validation split: extract 20% from training data and use it for validation
+# could also create a separate data (a list of tuples) for validation_data parameter
+model.fit(scaled_train_samples, train_labels, validation_split=0.20, batch_size=10,
+          epochs=20, shuffle=True, verbose=2)
+
+test_samples = []
+# Generate test samples
+for i in range(105):
+    random_younger = randint(13, 64)
+    test_samples.append(random_younger)
+
+    random_older = randint(65, 100)
+    test_samples.append(random_older)
+
+test_samples = np.array(test_samples)
+scaled_test_samples = scaler.fit_transform(test_samples.reshape(-1, 1))
+
+
+# Predict
+predictions = model.predict(scaled_test_samples, batch_size=10, verbose=0)
+
+for i in predictions:
+    print(i)
